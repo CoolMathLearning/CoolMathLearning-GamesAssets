4dc90f3c4e75343c76797233b3228b73250888cf
mrcoki1 <149107493+mrcoki1@users.noreply.github.com>
2024-01-27 10:06:10 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+function playGame1() {
+  // Redirect to Game 1 page
+  window.location.href = 'game1.html';
+}
+
+function playGame2() {
+  // Redirect to Game 2 page
+  window.location.href = 'game2.html';
+}
+
+// Add more functions for other games if needed
